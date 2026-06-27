5c6d5915b0cc37f8e7dc34785d582c0ed92e2b91
WAROL52 <rabetsyrolio@gmail.com>
2026-06-27 20:36:59 +0300

    initial commit

---
+++ packages/ui/src/components/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@code2-base-ui/ui/lib/utils";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-none bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
