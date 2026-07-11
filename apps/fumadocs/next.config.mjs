93c320f234c5b0f590f672617f709f574f8cc459
WAROL52 <rabetsyrolio@gmail.com>
2026-07-11 21:33:16 +0300

    chore(fumadocs): retirer code mort Fumadocs Story
    3 traces d'un feature scaffoldé mais jamais câblé :
    - src/lib/story.ts : exportait defineStory, 0 import
    - global.css : importait @fumadocs/story/css/preset.css
    - next.config.mjs : wrapper createNextStory() inutile
    - package.json : dépendance @fumadocs/story
    
    Supprimé : fichier story.ts, ligne CSS, wrapper next.config,
    dépendance npm.

--- apps/fumadocs/next.config.mjs
+++ apps/fumadocs/next.config.mjs
@@ -1,12 +1,10 @@
-import { createNextStory } from "@fumadocs/story/next";
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
-const withStory = createNextStory();
 
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
 };
 
-export default withStory(withMDX(config));
+export default withMDX(config);
--- apps/fumadocs/src/lib/story.ts
+++
@@ -1,3 +0,0 @@
-import { defineStoryFactory } from "@fumadocs/story/next/client";
-
-export const { defineStory } = defineStoryFactory();
